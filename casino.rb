b83e2fb81013aa88232e2e988c053c2e63002796
Nathan Stuart <nathan.stuart01@gmail.com>
2017-01-30 10:16:35 -0700

    started casino menu

--- casino.rb
+++ casino.rb
@@ -1,27 +1,26 @@
 require 'pry'
 require 'colorize'
 require 'artii'
-require_relative 'player' #copy paste 
+require_relative 'player' #copy paste
 
 class Casino
   #TO DO: HANDLE MULTI PLAYERS
-  #hint: think array 
+  #hint: think array
   attr_accessor :player
-  
+
   def initialize
     puts'Welcome To The Casino!'.colorize(:blue)
     @player = Player.new  #instance variable
     puts "What game do you want to play #{player.name}?"
-    menu 
-  #show casino game manu 
+    menu
+  #show casino game manu
   #let player choose a game
   #initialize the new game passing the player as a parameter
-  end 
+  end
 
   def menu
-
-  end 
+    puts '*** Casino Menu ***'
+  end
 end
 
 Casino.new
-
