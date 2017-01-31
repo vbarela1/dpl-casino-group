300f83998f18688bcba6623d583cf3b4059c9611
Valerie Barela <vbarela1@gmail.com>
2017-01-31 09:50:25 -0700

    vals newest

--- casino.rb
+++ casino.rb
@@ -1,90 +1,8 @@
-#game players bank rolls 
-#
 
 require 'pry'
 require 'colorize'
 require 'artii'
-# require_relative 'player' #copy paste
-
-class HighLow
-  attr_accessor :name, :bet
-
-  def initialize(player)
-    @player = player
-    puts "Welcome to High/Low #{player.name}!"
-    cards
-  end
-
-  def cards
-    win = 'Congratulations you have won $1.00'
-    lose = 'Bummer you just lost $1.00'
-    if(@player.wallet.amount <= 0)
-      puts 'You are out of money'
-      puts 'Go to the ATM'
-      puts 'how much money do you want to withdraw?'
-      withdraw = gets.chomp.to_i
-      @player.wallet = Wallet.new(withdraw)
-    end
-  puts 'Here is your first card:'
-  @card1 = 1 + rand(10)
-  puts @card1
-  puts 'Will the next card be higher or lower?'
-  @user_bet = gets.chomp
-  @card2 = 1 + rand(10)
-  puts @card2
-    case
-    when @card2 > @card1 && @user_bet == 'higher'
-      win
-      @player.wallet.amount = @player.wallet.amount + 1
-      puts "You have this much left #{@player.wallet.amount}"
-      cards
-    when @card2 < @card1 && @user_bet == 'lower'
-      win
-      @player.wallet.amount = @player.wallet.amount + 1
-      puts "You have this much left #{@player.wallet.amount}"
-      cards
-    when @card2 > @card1 && @user_bet == 'lower'
-      lose
-      @player.wallet.amount = @player.wallet.amount - 1
-      puts "You have this much left #{@player.wallet.amount}"
-      cards
-    when @card2 < @card1 && @user_bet == 'higher'
-      lose
-      @player.wallet.amount = @player.wallet.amount - 1
-      puts "You have this much left #{@player.wallet.amount}"
-      cards
-    else @card2 == @card1
-      puts 'Draw, try again'
-      puts "You have this much left #{@player.wallet.amount}"
-      cards
-    end
-  end
-end
-
-
-class Wallet
-  attr_accessor :amount
-
-  def initialize(amount)
-    #randomly give an amount
-    @amount = amount
- end
-end
-
-class Player
-  attr_accessor :name, :wallet
-
-  def initialize
-    puts 'What is your name player?'
-    @name = gets.strip
-    puts 'How much money are you playing with?'
-    @wallet = Wallet.new(gets.to_f) #instatuates a new wallet
-  end
-
-  def info
-    puts "Players Name Is: #{name} And Player Has: #{wallet.amount}"
-  end
-end
+require_relative 'player'
 
 class Casino
   #TO DO: HANDLE MULTI PLAYERS
@@ -113,7 +31,7 @@ class Casino
         player.info
         menu
       when 2
-        # Slots.new(player)
+        Slots.new(player)
         # menu
       when 3
         HighLow.new(player)
---
+++ slots.rb
@@ -0,0 +1,46 @@
+require_relative 'player'
+
+class Slots 
+  attr_accessor :type  
+
+  @slot = ['Cherry', 'Orange', 'Banana', 'Bar','Bell']
+
+  def initialize(player)
+      @player = player
+      puts "Welcome to Slots #{@player}!"
+      bet
+    end
+
+  def bet
+    puts 'How much would you like to bet?'
+    user_bet = gets.chomp_to.f
+    puts 'Activating slot machine.'
+    spin 
+  end 
+
+end 
+
+@slots = Slots.new(@player)
+
+#  if s1==s2 && s2==s3
+#     3
+#   elsif s1==s2 || s2==s3 || s1==s3
+#     2
+#   else
+#     0
+#   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
